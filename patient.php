7963e4f6bd451d7ed2d4eca88d22d78626d0c10f
Sabri Barbaria <sabribarbar@gmail.com>
2020-10-11 14:18:15 +0100

    complet

--- patient.php
+++ patient.php
@@ -20,40 +20,22 @@ session_start();
     
     $file = $_FILES["file"];
     move_uploaded_file($file["tmp_name"],"$updirim".$file["name"]);
-    require_once('class_dicom.php');
-  
-  $file = (isset($argv[1]) ? $argv[1] : "$updirim".$file["name"]);
-  
-  if(!$file) {
-    print "USAGE: ./dcm_to_jpg.php <FILE>\n";
-    exit;
-  }
-  
-  if(!file_exists($file)) {
-    print "$file: does not exist\n";
-    exit;
-  }
-  
-  $job_start = time();
-  
-  $d = new dicom_convert;
-  $d->file = $file;
-  $d->dcm_to_jpg();
+
   
   }
   if(isset($_POST['updoc'])){
     
-    $filed = $_FILES["filed"];
+    $filed = $_FILES["file"];
     move_uploaded_file($filed["tmp_name"],"$updirdoc".$filed["name"]);
   }
   if(isset($_POST['upzip'])){
     
-    $filed = $_FILES["filed"];
+    $filed = $_FILES["file"];
     move_uploaded_file($filed["tmp_name"],"$updirzip".$filed["name"]);
   }
   if(isset($_POST['upvid'])){
     
-    $filed = $_FILES["filed"];
+    $filed = $_FILES["file"];
     move_uploaded_file($filed["tmp_name"],"$updirvid".$filed["name"]);
   }
   $title = 'Dossier Patient';
